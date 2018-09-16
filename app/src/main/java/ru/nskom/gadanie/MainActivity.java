58ee220b72c0f2b0ea642f52a58d6a349ed506b1
nskom <omrecordzz@gmail.com>
2018-09-16 20:15:29 +0700

    newl

--- app/src/main/java/ru/nskom/gadanie/MainActivity.java
+++ app/src/main/java/ru/nskom/gadanie/MainActivity.java
@@ -1,6 +1,12 @@
 package ru.nskom.gadanie;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +19,18 @@ public class MainActivity extends AppCompatActivity {
     private static final int NUM_ROWS = 3;
     private static final int NUM_COLS = 9;
 
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         populateButtons();
-
     }
 
     private void populateButtons() {
-        String[][] bnt_name = {{"1", "2", "3", "4", "5", "6", "7", "8", "9"},
+        String[][] btn_name = {{"1", "2", "3", "4", "5", "6", "7", "8", "9"},
                 {"1", "1", "1", "2", "1", "3", "1", "4", "1"},
                 {"5", "1", "6", "1", "7", "1", "8", "1", "9"}};
         TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
@@ -41,27 +48,73 @@ public class MainActivity extends AppCompatActivity {
             table.addView(tableRow);
 
             for (int col = 0; col < NUM_COLS; col++) {
+                final int FINAL_COL = col;
+                final int FINAL_ROW = row;
                 Button button = new Button(this);
                 button.setLayoutParams(new TableRow.LayoutParams(
                         TableRow.LayoutParams.MATCH_PARENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f));
+                button.setText(btn_name[row][col]);
+                button.setId(row + col);
+                button.setTag(row + col);
 
-                button.setText(bnt_name[row][col]);
+                //Убираем отступы
+                button.setPadding(0, 0, 0, 0);
 
                 button.setOnClickListener(new View.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
                     @Override
                     public void onClick(View view) {
-                        gridButtonClicked();
+                        gridButtonClicked(FINAL_COL, FINAL_ROW);
                     }
                 });
 
                 tableRow.addView(button);
+                buttons[row][col] = button;
             }
         }
     }
 
-    private void gridButtonClicked() {
-        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void gridButtonClicked(int col, int row) {
+        Toast.makeText(this, "Button clicked: " + row + "," + col, Toast.LENGTH_SHORT).show();
+
+        Button button = buttons[row][col];
+
+        //Ограничение изменения размера кнопки
+        lockButtonSizes();
+
+        //заливаем фоном нажатую кнопку
+        bitmap(button);
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void bitmap(Button button) {
+        //заливаем фоном нажатую кнопку
+        int newWidth = button.getWidth();
+        int newHeight = button.getHeight();
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.lock);
+        Bitmap scaleBitmap = Bitmap.createScaledBitmap(originalBitmap, newWidth, newHeight, true);
+        Resources resource = getResources();
+        button.setBackground(new BitmapDrawable(resource, scaleBitmap));
+    }
+
+    private void lockButtonSizes() {
+        //Ограничение изменения размера кнопки
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                Button button = buttons[row][col];
+
+                int width = button.getWidth();
+                button.setMinWidth(width);
+                button.setMaxWidth(width);
+
+                int height = button.getHeight();
+                button.setMinHeight(height);
+                button.setMaxHeight(height);
+            }
+        }
     }
 }
